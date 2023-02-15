502f1a377c35049e01394b397b892b3b50983967
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-02-15 13:05:24 -0800

    Adding LeetCode Two Sums Challenge

---
+++ twoSums.js
@@ -0,0 +1,17 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+var twoSum = function (nums, target) {
+  const map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i]
+    if (map.has(complement)) {
+      return [map.get(complement), i]
+    }
+    map.set(nums[i], i)
+  }
+  return []
+}
