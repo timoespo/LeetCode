2b2fe1a03bef6d2302a3523b438e9dacff9ce942
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-05-16 10:00:19 -0700

    Adding Leetcodes Word Break Challenge

---
+++ wordBreak.js
@@ -0,0 +1,36 @@
+// Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+// Example 1:
+
+// Input: s = "leetcode", wordDict = ["leet","code"]
+// Output: true
+// Explanation: Return true because "leetcode" can be segmented as "leet code".
+// Example 2:
+
+// Input: s = "applepenapple", wordDict = ["apple","pen"]
+// Output: true
+// Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+// Note that you are allowed to reuse a dictionary word.
+// Example 3:
+
+// Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+// Output: false
+
+function wordBreak(s, wordDict) {
+  const n = s.length
+  const dp = new Array(n + 1).fill(false)
+
+  dp[0] = true
+
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dp[j] && wordDict.includes(s.substring(j, i))) {
+        dp[i] = true
+        break
+      }
+    }
+  }
+  return dp[n]
+}
