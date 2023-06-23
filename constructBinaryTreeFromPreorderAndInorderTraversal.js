52b819ce9f773c49712060623c36e8db3f58e888
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-06-23 11:00:40 -0700

    Adding Leetcodes Construct Binary Tree from Inorder and Postorder Traversal Challenge

---
+++ constructBinaryTreeFromPreorderAndInorderTraversal.js
@@ -0,0 +1,22 @@
+// Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+// Example 1:
+
+// Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+// Output: [3,9,20,null,null,15,7]
+// Example 2:
+
+// Input: preorder = [-1], inorder = [-1]
+// Output: [-1]
+
+function buildTree(preorder, inorder) {
+  if (!preorder.length || !inorder.length) return null
+
+  let root = new TreeNode(preorder[0])
+  let mid = inorder.indexOf(root.val)
+
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid))
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1))
+
+  return root
+}
